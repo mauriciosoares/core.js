6df045ca382632be5ac211201380c8a287c00edd
Mauricio Soares <msoliveira@sp.r7.com>
2014-10-10 17:10:16 -0300

    spliting into files

---
+++ js/core.js
@@ -0,0 +1,44 @@
+(function(root) {
+  var Core = function() {
+    this.modules = {};
+  };
+
+  Core.prototype.register = function(module, constructor) {
+    this.modules[module] = {
+      constructor: constructor,
+      instance: null
+    };
+  };
+
+  Core.prototype.start = function(module) {
+    var module = this.modules[module];
+
+    module.instance = new module.constructor();
+    module.instance.init();
+  };
+
+  Core.prototype.startAll = function() {
+    for(var module in this.modules) {
+      if(this.modules.hasOwnProperty(module)) {
+        this.start(module);
+      }
+    }
+  };
+
+  Core.prototype.stop = function(module) {
+    var module = this.modules[module];
+
+    module.instance.destroy();
+    module.instance = null;
+  };
+
+  Core.prototype.stopAll = function() {
+    for(var module in this.modules) {
+      if(this.modules.hasOwnProperty(module)) {
+        this.stop(module);
+      }
+    }
+  };
+
+  root.Core = new Core();
+} (window));
