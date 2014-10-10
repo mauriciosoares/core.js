331eeba77c1b7df835e20eeedb8a32c3ac18b620
Mauricio Soares <msoliveira@sp.r7.com>
2014-10-10 17:12:54 -0300

    start notification in sandbox

--- js/sandbox.js
+++ js/sandbox.js
@@ -3,5 +3,10 @@
 
   };
 
+  Sandbox.prototype.notify = function(notification) {
+    console.log(notification.type);
+    console.log(notification.data);
+  };
+
   root.Sandbox = Sandbox;
 } (window));
