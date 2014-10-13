f7ce75f54906bac0e630bbc82e9ca62da5d6d7f6
msodeveloper <m.soares2603@gmail.com>
2014-10-13 09:52:06 -0300

    add bump configs

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,5 +20,22 @@ module.exports = function(grunt) {
               '<%= grunt.template.today(\'yyyy-mm-dd\') %>\n' +
               '* Copyright (c) <%= grunt.template.today(\'yyyy\') %> <%= pkg.author %>;\n' +
               '* Licensed <%= pkg.license %> \n*/\n\n'
-  }
+  };
+
+  // =============================================
+  // bump
+  config.bump = {};
+  config.bump.options = {
+    files: ['package.json'],
+    updateConfigs: [],
+    commit: true,
+    commitMessage: 'Release v%VERSION%',
+    commitFiles: ['package.json'],
+    createTag: true,
+    tagName: 'v%VERSION%',
+    tagMessage: 'Version %VERSION%',
+    push: true,
+    pushTo: 'https://github.com/msodeveloper/core.js.git',
+    gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
+  };
 };
