cfb7fc75f8b87381645854d77372d1b48eb44dc1
Mauricio Soares <m.soares2603@gmail.com>
2014-12-25 20:21:41 -0200

    updating sandbox specs

--- tests/sandbox/sandboxSpec.js
+++ tests/sandbox/sandboxSpec.js
@@ -1,7 +1,32 @@
 describe('Testing Sandbox', function() {
+  afterEach(function() {
+    Core.stopAll();
+    Core.modules = {};
+  });
+
+  it('Should return an instance of sandbox as parameter inside modules', function() {
+    Core.register('tweet', function(sandbox) {
+      return {
+        init: function() {
+          expect(sandbox instanceof Sandbox).toBeTruthy();
+        }
+      }
+    });
+
+    Core.start('tweet');
+  });
+
   it('Should listen to notification from other modules');
 
   it('Should notify other modules from a specific notification');
 
+  it('Should pass data through notifications');
+
   it('Should give access to a extension inside a module');
+
+  it('Should not overwrite an existing listening notification');
+
+  it('Should overwrite an existing notification if force parameter is passed');
+
+  it('Should listen to multiple notification if its used as array');
 });
