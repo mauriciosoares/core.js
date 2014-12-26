34c2a2cbd74d9b341ae94626fb0113af3c1b6d6a
Mauricio Soares <m.soares2603@gmail.com>
2014-12-26 11:35:19 -0200

    updating notification tests

--- tests/sandbox/sandboxSpec.js
+++ tests/sandbox/sandboxSpec.js
@@ -37,7 +37,7 @@ describe('Testing Sandbox', function() {
     Core.register('tweet-list', function(sandbox) {
       return {
         init: function() {
-          sandbox.listen('new-tweet', this.newTweet)
+          sandbox.listen('new-tweet', this.newTweet);
         },
 
         newTweet: function() {
@@ -78,7 +78,7 @@ describe('Testing Sandbox', function() {
     Core.register('tweet-list', function(sandbox) {
       return {
         init: function() {
-          sandbox.listen(['new-tweet', 'new-tweet2'], this.newTweet)
+          sandbox.listen(['new-tweet', 'new-tweet2'], this.newTweet);
         },
 
         newTweet: function() {
@@ -116,8 +116,8 @@ describe('Testing Sandbox', function() {
     Core.register('tweet-list', function(sandbox) {
       return {
         init: function() {
-          sandbox.listen('new-tweet', this.newTweet)
-          sandbox.listen('new-tweet2', this.newTweet2)
+          sandbox.listen('new-tweet', this.newTweet);
+          sandbox.listen('new-tweet2', this.newTweet2);
         },
 
         newTweet: function() {
@@ -137,11 +137,155 @@ describe('Testing Sandbox', function() {
     expect(spying.newTweet2).not.toHaveBeenCalled();
   });
 
-  it('Should pass data through notifications');
+  it('Should pass data through notifications', function() {
+    Core.register('tweet', function(sandbox) {
+      return {
+        init: function() {
+          sandbox.notify({
+            type: 'new-tweet',
+            data: {
+              a: true,
+              b: false
+            }
+          });
+        }
+      }
+    });
+
+    Core.register('tweet-list', function(sandbox) {
+      return {
+        init: function() {
+          sandbox.listen('new-tweet', this.newTweet);
+        },
+
+        newTweet: function(data) {
+          expect(data.a).toBeTruthy();
+          expect(data.b).toBeFalsy();
+        }
+      }
+    });
+
+    Core.start('tweet-list');
+    Core.start('tweet');
+  });
+
+  it('Should not overwrite an existing listening notification', function() {
+    var spying = {
+      toBeCalled: function() {},
+      notToBeCalled: function() {}
+    };
+
+    spyOn(spying, 'toBeCalled');
+    spyOn(spying, 'notToBeCalled');
+
+    Core.register('tweet', function(sandbox) {
+      return {
+        init: function() {
+          sandbox.notify({
+            type: 'new-tweet',
+            data: {}
+          });
+        }
+      }
+    });
+
+    Core.register('tweet-list', function(sandbox) {
+      return {
+        init: function() {
+          sandbox.listen('new-tweet', this.toBeCalled);
+          sandbox.listen('new-tweet', this.notToBeCalled);
+        },
+
+        toBeCalled: function() {
+          spying.toBeCalled();
+        },
+
+        notToBeCalled: function() {
+          spying.notToBeCalled();
+        }
+      }
+    });
+
+    Core.start('tweet-list');
+    Core.start('tweet');
+
+    expect(spying.toBeCalled).toHaveBeenCalled();
+    expect(spying.notToBeCalled).not.toHaveBeenCalled();
+  });
+
+  it('Should overwrite an existing notification if force parameter is passed', function() {
+    var spying = {
+      toBeCalled: function() {},
+      toOverwrite: function() {}
+    };
+
+    spyOn(spying, 'toBeCalled');
+    spyOn(spying, 'toOverwrite');
+
+    Core.register('tweet', function(sandbox) {
+      return {
+        init: function() {
+          sandbox.notify({
+            type: 'new-tweet',
+            data: {}
+          });
+        }
+      }
+    });
+
+    Core.register('tweet-list', function(sandbox) {
+      return {
+        init: function() {
+          sandbox.listen('new-tweet', this.toBeCalled);
+          sandbox.listen('new-tweet', this.toOverwrite, this, true);
+        },
+
+        toBeCalled: function() {
+          spying.toBeCalled();
+        },
+
+        toOverwrite: function() {
+          spying.toOverwrite();
+        }
+      }
+    });
+
+    Core.start('tweet-list');
+    Core.start('tweet');
+
+    expect(spying.toBeCalled).not.toHaveBeenCalled();
+    expect(spying.toOverwrite).toHaveBeenCalled();
+  });
+
+  it('Should be able to choose the "this" value inside an notification', function() {
+    var thisReference = {};
+
+    Core.register('tweet', function(sandbox) {
+      return {
+        init: function() {
+          sandbox.notify({
+            type: 'new-tweet',
+            data: {}
+          });
+        }
+      }
+    });
+
+    Core.register('tweet-list', function(sandbox) {
+      return {
+        init: function() {
+          sandbox.listen('new-tweet', this.newTweet, thisReference);
+        },
+
+        newTweet: function() {
+          expect(this).toBe(thisReference);
+        }
+      }
+    });
+
+    Core.start('tweet-list');
+    Core.start('tweet');
+  });
 
   it('Should give access to a extension inside a module');
-
-  it('Should not overwrite an existing listening notification');
-
-  it('Should overwrite an existing notification if force parameter is passed');
 });
