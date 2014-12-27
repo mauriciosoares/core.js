086440591ab87c558762b86a065c51bafc302d3e
Mauricio Soares <m.soares2603@gmail.com>
2014-12-27 11:49:45 -0200

    docs extend code

--- dist/core.js
+++ dist/core.js
@@ -1,13 +1,25 @@
-/** core.js - v0.0.3 - 2014-12-08
+/** core.js - v0.0.3 - 2014-12-27
 * Copyright (c) 2014 Mauricio Soares;
 * Licensed MIT 
 */
 
 (function(root) {
+  /**
+  * The constructor of Core
+  *
+  * @method Core
+  */
   var Core = function() {
     this.modules = {};
   };
 
+  /**
+  * Registers a new module
+  *
+  * @method register
+  * @param {string} module the name of the new module
+  * @param {function} constructor the constructor of the new module
+  */
   Core.prototype.register = function(module, constructor) {
     this.modules[module] = {
       constructor: constructor,
@@ -15,13 +27,26 @@
     };
   };
 
+  /**
+  * Check if the module already exists
+  *
+  * @method moduleExist
+  * @param {string} module the name of the module that will be checked
+  * @param {boolean} destroy check if the module exists, but is already destroyed
+  * @return {boolean} if the module exists or already have an instance
+  */
   Core.prototype.moduleExist = function(module, destroy) {
-    // this.helpers.Error('!module', module);
     if(destroy) return !module || !module.instance;
 
     return !module || module.instance;
   };
 
+  /**
+  * Starts a registered module
+  *
+  * @method start
+  * @param {string} module the name of the module
+  */
   Core.prototype.start = function(module) {
     var cModule = this.modules[module];
 
@@ -32,6 +57,12 @@
     if(cModule.instance.init) cModule.instance.init();
   };
 
+  /**
+  * Stops a registered module
+  *
+  * @method start
+  * @param {string} module the name of the module
+  */
   Core.prototype.stop = function(module) {
     var cModule = this.modules[module];
 
@@ -44,14 +75,30 @@
     root.Sandbox.clearNotifications(module);
   };
 
+  /**
+  * Start all uninstarted modules
+  *
+  * @method startAll
+  */
   Core.prototype.startAll = function() {
     this.xAll('start');
   };
 
+  /**
+  * Stop all started modules
+  *
+  * @method stopAll
+  */
   Core.prototype.stopAll = function() {
     this.xAll('stop');
   };
 
+  /**
+  * Helper for startAll and stopAll
+  *
+  * @method xAll
+  * @param {string} method the method that will be triggered
+  */
   Core.prototype.xAll = function(method) {
     for(var module in this.modules) {
       if(this.modules.hasOwnProperty(module)) this[method](module);
@@ -97,10 +144,24 @@
 
   var extensions = {};
 
+  /**
+  * Extends core functionalities
+  *
+  * @method extend
+  * @param {string} name the name of the extension
+  * @param {function | array | boolean | string | number} implementation what the extension does
+  */
   var extend = function(name, implementation) {
     extensions[name] = implementation;
   };
 
+  /**
+  * returns the extension
+  *
+  * @method getExtension
+  * @param {string} extension the name of the extension
+  * @return {function | array | boolean | string | number} the implementation of the extension
+  */
   var getExtension = function(extension) {
     return extensions[extension] || null;
   };
--- src/core/methods/extend.js
+++ src/core/methods/extend.js
@@ -3,10 +3,24 @@
 
   var extensions = {};
 
+  /**
+  * Extends core functionalities
+  *
+  * @method extend
+  * @param {string} name the name of the extension
+  * @param {function | array | boolean | string | number} implementation what the extension does
+  */
   var extend = function(name, implementation) {
     extensions[name] = implementation;
   };
 
+  /**
+  * returns the extension
+  *
+  * @method getExtension
+  * @param {string} extension the name of the extension
+  * @return {function | array | boolean | string | number} the implementation of the extension
+  */
   var getExtension = function(extension) {
     return extensions[extension] || null;
   };
