4cf01e10c5a02110862dbb66f94e107a00830751
Mauricio Soares <m.soares2603@gmail.com>
2014-12-27 11:47:24 -0200

    docs in core code

--- src/core/core.js
+++ src/core/core.js
@@ -1,8 +1,20 @@
 (function(root) {
+  /**
+  * The constructor of Core
+  *
+  * @method Core
+  */
   var Core = function() {
     this.modules = {};
   };
 
+  /**
+  * Registers a new module
+  *
+  * @method register
+  * @param {string} module the name of the new module
+  * @param {function} constructor the constructor of the new module
+  */
   Core.prototype.register = function(module, constructor) {
     this.modules[module] = {
       constructor: constructor,
@@ -10,13 +22,26 @@
     };
   };
 
+  /**
+  * Check if the module already exists
+  *
+  * @method moduleExist
+  * @param {string} module the name of the module that will be checked
+  * @param {boolean} destroy check if the module exists, but is already destroyed
+  * @return {boolean} if the module exists or already have an instance
+  */
   Core.prototype.moduleExist = function(module, destroy) {
-    // this.helpers.Error('!module', module);
     if(destroy) return !module || !module.instance;
 
     return !module || module.instance;
   };
 
+  /**
+  * Starts a registered module
+  *
+  * @method start
+  * @param {string} module the name of the module
+  */
   Core.prototype.start = function(module) {
     var cModule = this.modules[module];
 
@@ -27,6 +52,12 @@
     if(cModule.instance.init) cModule.instance.init();
   };
 
+  /**
+  * Stops a registered module
+  *
+  * @method start
+  * @param {string} module the name of the module
+  */
   Core.prototype.stop = function(module) {
     var cModule = this.modules[module];
 
@@ -39,14 +70,30 @@
     root.Sandbox.clearNotifications(module);
   };
 
+  /**
+  * Start all uninstarted modules
+  *
+  * @method startAll
+  */
   Core.prototype.startAll = function() {
     this.xAll('start');
   };
 
+  /**
+  * Stop all started modules
+  *
+  * @method stopAll
+  */
   Core.prototype.stopAll = function() {
     this.xAll('stop');
   };
 
+  /**
+  * Helper for startAll and stopAll
+  *
+  * @method xAll
+  * @param {string} method the method that will be triggered
+  */
   Core.prototype.xAll = function(method) {
     for(var module in this.modules) {
       if(this.modules.hasOwnProperty(module)) this[method](module);
