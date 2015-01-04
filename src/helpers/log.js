9d17e2f78bf6fab1144a494455f50472c23f7a9e
Mauricio Soares <m.soares2603@gmail.com>
2015-01-04 00:47:18 -0200

    add log to Core

---
+++ src/helpers/log.js
@@ -0,0 +1,11 @@
+(function(Core, root) {
+  /**
+  * Adds console.log to Core helpers
+  *
+  * @method log
+  */
+  var log = (root.console) ? root.console.log.bind(root.console) : function() {};
+
+  Core.helpers = Core.helpers || {};
+  Core.helpers.log = log;
+} (this.Core, this));
