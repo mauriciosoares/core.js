6298f6d1a80ff743109808eb7ed63a3078b608d6
Mauricio Soares <m.soares2603@gmail.com>
2015-01-10 16:28:59 -0200

    updates error helper

--- src/helpers/error.js
+++ src/helpers/error.js
@@ -1,11 +1,18 @@
 (function(Core) {
+  /**
+  * Handles error messages
+  *
+  * @method err
+  * @param {string} error the type of the error
+  * @param {function} message the complementary message to the error
+  */
   var err = function(error, message) {
     Core.helpers.log(err.messages[error] + message);
   };
 
   err.messages = {
-    '!module': 'There\'s no module called: ',
-    'moduleExist': 'There\'s already a module called: '
+    '!module': 'There\'s no module named: ',
+    'moduleExist': 'Can\'t register an already registered module: '
   };
 
   Core.helpers = Core.helpers || {};
