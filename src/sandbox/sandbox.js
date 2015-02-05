e400c37c4528800f4afbac12b8174a22d5d7b1a3
Mauricio Soares <m.soares2603@gmail.com>
2015-02-05 21:27:13 -0200

    renaming `x` to `get`

--- src/sandbox/sandbox.js
+++ src/sandbox/sandbox.js
@@ -89,7 +89,7 @@ Sandbox.prototype.addNotification = function(notification, callback, context, re
 * @param {string} extension the name of the extension
 * @return {function | array | boolean | string | number} the implementation of the extension
 */
-Sandbox.prototype.x = function(extension) {
+Sandbox.prototype.use = function(extension) {
   return Core.getExtension(extension);
 };
 
