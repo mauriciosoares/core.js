761cfa94be59766c79cd6a06b3a04e5aae85cb2b
Cyril Walle <cyril.walle@protonmail.com>
2020-05-09 02:15:23 +0200

    clean

--- examples/draw/draw.js
+++ examples/draw/draw.js
@@ -8,7 +8,6 @@ const width  = 800;
 const height = 600;
 
 const start = function (emitter) {
-
   const canvas = document.getElementById(`canvas`);
   const context = canvas.getContext('2d');
   canvas.width  = width;
@@ -37,36 +36,3 @@ const stop = function (instance) {
     const { context } = instance;
     context.fillRect(0, 0, width, height);
 };
-
-// context.arc(10, 10, 30, 0, Math.PI * 2);
-
-
-// context.clearRect(0, 0, canvas.width, canvas.height);
-// context.fillStyle = '#000000';
-// context.fillRect(0, 0, 400, 400);
-// context.fillStyle = `#de2321`; // red
-// context.strokeStyle = `#009900`; // red
-
-
-// context.beginPath();
-
-// context.moveTo(10, 10);
-// context.arc(10, 10, 30, 0, Math.PI * 2);
-
-// context.stroke();
-// context.moveTo(10, 10);
-// context.lineTo(100, 100);
-// context.lineTo(100, 200);
-// context.fill();
-
-
-// context.fillRect(
-//     10,
-//     10,
-//     450,
-//     450
-// );
-// context.fill();
-// context.stroke();
-
-//context.clearRect(0, 0, canvas.width, canvas.height);
