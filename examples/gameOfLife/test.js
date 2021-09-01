7c32b45967041d8029a69d597066f3dbfda3db7b
Cyril Walle <cyril.walle@protonmail.com>
2021-09-01 22:50:16 +0200

    Update test.js

--- examples/gameOfLife/test.js
+++ examples/gameOfLife/test.js
@@ -1,17 +1,20 @@
 // @ts-check
-// export { start, stop, getState, restoreState };
+export { start };
 // import { WANT_DRAW, TICK, WANTS_TOGGLE, WANTS_SAVE, SAVE, LOAD } from "./eventNames.js";
+// import { WANT_DRAW, TICK, WANTS_TOGGLE, WANTS_SAVE, SAVE, LOAD } from "http://localhost:8080/examples/gameOfLife/eventNames.js";
+// import { WANT_DRAW, TICK, WANTS_TOGGLE, WANTS_SAVE, SAVE, LOAD } from "/examples/gameOfLife/eventNames.js";
 
 // import { initialGrid, alive, dead, size } from "./settings/grid.js";
 // import {deepCopy} from "./dependencies.js";
 
 const start = function (emitter) {
-  console.log("start test.js")
+  console.log(import.meta.url);
   const instance = {
 
   };
 
-  
+  emitter.emit("x", "from test.js")
+  emitter.on("WANT_DRAW", console.log)
   return instance;
 };
 
